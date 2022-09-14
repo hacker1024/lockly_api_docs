c9b592705e1f067390a0c1bbf3ebe9c29e94accc
hacker1024 <hacker1024@users.sourceforge.net>
2022-09-15 03:27:54 +1000

    fix: Remove duplicate "documentation" in page title

--- docs/conf.py
+++ docs/conf.py
@@ -21,5 +21,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_title = "Unofficial Lockly API Documentation"
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
